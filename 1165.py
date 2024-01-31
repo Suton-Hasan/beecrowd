fae01ecf753a7f915ba76bdfbb12fa3d849d0f92
Md Abu Bokar Siddike <126772251+Suton-Hasan@users.noreply.github.com>
2024-01-31 15:56:19 +0600

    Add files via upload

--- 1165.py
+++ 1165.py
@@ -1,12 +1,11 @@
-N=int(input())
-for i in range(N):
-    X=int(input())
+n=int(input())
+for i in range(n):
+    a=int(input())
     c=0
-    for j in range(1,X):
-        if(X%j==0):
+    for j in range(1,a+1):
+        if(a%j==0):
             c=c+1
-            print(j)
-            if (c>2):
-                print(X,"nao eh primo")
-            else:
-                print(X,"eh primo")
+    if(c==2):
+        print("{} eh primo".format(a))
+    else:
+        print("{} nao eh primo".format(a))
